e9c97506747406b6afa90f3fd74755bffaefa251
sragfw <fw@studer-raimann.ch>
2020-10-21 11:24:18 +0200

    Fix

--- src/build.php
+++ src/build.php
@@ -3,7 +3,7 @@
 $yml = file_get_contents(__DIR__ . "/auto_version_tag_ci.yml");
 $php = file_get_contents(__DIR__ . "/auto_version_tag_ci.php");
 
-$build = str_replace("%PHP%", escapeshellarg(trim($php)), $yml);
+$build = str_replace("%BASE64_PHP%", base64_encode(ltrim(trim($php),"<?php")), $yml);
 
 mkdir(__DIR__ . "/../build", null, true);
 file_put_contents(__DIR__ . "/../build/auto_version_tag_ci.yml", $build);
