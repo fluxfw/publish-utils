b0f2337679a895a867b9d9e9ca07f60aa86fd2c0
fluxfw <fluxfw@fluxlabs.ch>
2021-06-04 09:40:41 +0200

    Fix

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -3,17 +3,12 @@ set -e
 
 echo "Generate nginx config"
 if [ -n "$NGINX_HTTPS_CERT" ]; then
-  echo "With https"
   if [ "$NGINX_HTTPS_PORT" != "443" ]; then
     https_redirect_port=:$NGINX_HTTPS_PORT
-  else
-    https_redirect_port=
   fi
   if [ -n "$NGINX_HTTPS_DHPARAM" ]; then
     ssl_dhparam="
 	ssl_dhparam $NGINX_HTTPS_DHPARAM;"
-  else
-    ssl_dhparam=
   fi
   listen="listen $NGINX_LISTEN:$NGINX_HTTP_PORT;
   return 301 https://\$host$https_redirect_port\$request_uri;
@@ -23,13 +18,14 @@ server {
 	ssl_certificate $NGINX_HTTPS_CERT;
 	ssl_certificate_key $NGINX_HTTPS_KEY;$ssl_dhparam"
 else
-  echo "Without https"
   listen="listen $NGINX_LISTEN:$NGINX_HTTP_PORT;"
 fi
-echo "server_tokens $NGINX_SERVER_TOKENS;
+echo "server_tokens off;
+
 types {
 	text/plain	yml;
 }
+
 server {
 	index index.html;
 
