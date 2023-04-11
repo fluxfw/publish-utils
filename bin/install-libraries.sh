3ea89b44d9023553770dc60dd99c19218c012e5a
fluxfw <fluxfw@fluxlabs.ch>
2023-04-11 08:49:02 +0200

    libraries

--- bin/install-libraries.sh
+++ bin/install-libraries.sh
@@ -19,9 +19,9 @@ installLibrary() {
 
 checkAlreadyInstalled
 
-installLibrary flux-config-api https://github.com/fluxfw/flux-config-api/archive/refs/tags/v2023-03-20-1.tar.gz
+installLibrary flux-config-api https://github.com/fluxfw/flux-config-api/archive/refs/tags/v2023-04-11-1.tar.gz
 
-installLibrary flux-http-api https://github.com/fluxfw/flux-http-api/archive/refs/tags/v2023-04-03-1.tar.gz
+installLibrary flux-http-api https://github.com/fluxfw/flux-http-api/archive/refs/tags/v2023-04-11-1.tar.gz
 
 installLibrary flux-shutdown-handler https://github.com/fluxfw/flux-shutdown-handler/archive/refs/tags/v2023-03-16-1.tar.gz
 
