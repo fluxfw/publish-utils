1590e302744d0da932bf4e1685fa833a22248aea
fluxfw <fluxfw@bluewin.ch>
2023-12-04 17:20:19 +0100

    Fix

--- bin/install-libraries.sh
+++ bin/install-libraries.sh
@@ -59,7 +59,7 @@ installArchiveLibrary flux-config https://github.com/fluxfw/flux-config/archive/
 
 installArchiveLibrary flux-http https://github.com/fluxfw/flux-http/archive/refs/tags/v2023-12-04-1.tar.gz
 
-installArchiveLibrary flux-pwa-generator https://github.com/fluxfw/flux-pwa-generator/archive/refs/tags/v2023-12-04-3.tar.gz
+installArchiveLibrary flux-pwa-generator https://github.com/fluxfw/flux-pwa-generator/archive/refs/tags/v2023-12-04-4.tar.gz
 
 installArchiveLibrary flux-shutdown-handler https://github.com/fluxfw/flux-shutdown-handler/archive/refs/tags/v2023-03-16-1.tar.gz
 
